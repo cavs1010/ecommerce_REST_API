582ef712cefe1dedc12384242c0bb6d0be54c9d7
Camilo <cavs1010@gmail.com>
2023-01-08 14:37:20 -0500

    Cart.js initialization

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const productRouter = require("./routes/product");
 const customerRouter = require("./routes/customer");
+const cartRouter = require("./routes/cart");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -13,6 +14,7 @@ app.use(bodyParser.json());
 
 app.use("/product", productRouter);
 app.use("/customer", customerRouter);
+app.use("/cart", cartRouter);
 
 app.get("/", (req, res) => {
   res.json({ info: "It is ready to use" });
---
+++ routes/cart.js
@@ -0,0 +1,8 @@
+/*---IMPORTS---*/
+const express = require("express");
+const cartRouter = express.Router();
+
+const db = require("../db");
+
+/*---ROUTER EXPORT---*/
+module.exports = cartRouter;
